b76d07b5b4be36cc43fcb8b3a301708392b4358d
Jim Nanney <jnanney@gmail.com>
2013-07-17 20:39:28 -0500

    wip

--- lib/onstar/client.rb
+++ lib/onstar/client.rb
@@ -1,7 +1,24 @@
-require "onstar/client/version"
+require 'onstar/client/version'
+require 'httparty'
 
 module Onstar
-  module Client
+  def Client
     # Your code goes here...
+
+    include HTTParty
+    base_uri = 'https://developer.gm.com'
+    headers 'Accept' => 'application/json'
+
+    def initialize(key, secret)
+      @auth = { api_key: key, secret: secret }
+    end
+
+
+    def get_access_token
+      options.merge!( { basic_auth: @auth } )
+      self.class.get('/api/v1/oauth/access_token', @auth)
+    end
+
+
   end
 end
--- spec/onstar/client_spec.rb
+++ spec/onstar/client_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
+API_KEY = ENV['GM_API_KEY']
+SECRET = ENV['GM_SECRET_KEY']
+
 describe Onstar::Client do
+  subject { described_class.new(API_KEY, SECRET) }
+
   it 'should have a version number' do
     Onstar::Client::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
+  it 'should get an access token upon creation' do
+    subject.get_auth_token
+    subject.should_not be_nil 
+  end
+
+  xit 'should do something useful' do
     false.should be_true
   end
 end
